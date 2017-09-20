11308bd095a5295f815add7fd68782bd629df978
hdt <p4ngb0y@gmail.com>
2017-09-20 19:52:49 +0800

    addTerm fix & toString

--- lab1/Polynomial.java
+++ lab1/Polynomial.java
@@ -17,9 +17,10 @@ public class Polynomial {
     // add a single term to the polynomial
     public void addTerm(Term term) {
         Term i = this.head;
-        while (i.next.coef > term.coef)
+        while (i.next != null && i.next.exp > term.exp) {
             i = i.next;
-        if (i.next.coef == term.coef)
+        }
+        if (i.next != null && i.next.exp == term.exp)
             i.next.coef += term.coef;
         else {
             term.next = i.next;
@@ -34,22 +35,30 @@ public class Polynomial {
     // example: 4.0x^3+3.2x^2-2.1x^1+1.0x^0
     // example: -12.0x^9-1.0x^7+3.0x^5+10.0x^2+5.0x^0
     public String toString() {
-        String s = "";
         Formatter fmt = new Formatter();
-        for (Term t = head; t != null; t = t.next) {
-            s += fmt.format(
+        for (Term t = getFirst(); t != null; t = t.next) {
+            fmt.format(
                 "%c%.1fx^%d", 
-                (t != head && t.coef > 0) ? '+':'\0',
+                (t != getFirst() && t.coef > 0) ? '+':'\0',
                 t.coef,
                 t.exp
             );
         }
-        return s;
+        return fmt.toString();
     }
     // write your own code to test your implementation
     public static void main(String[] args) {
         Term a = new Term(4, 3);
+        Term b = new Term(4, 3);
+        Term c = new Term(3, 2);
+        Term d = new Term(-3, 5);
         Polynomial p = new Polynomial(a);
+        System.out.println(p);
+        p.addTerm(b);
+        System.out.println(p);
+        p.addTerm(c);
+        System.out.println(p);
+        p.addTerm(d);
         System.out.println(p);
     }
 }
