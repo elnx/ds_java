b830a84f9b44724bafcd8e9fe846c6b30898cc60
hdt <p4ngb0y@gmail.com>
2017-09-27 15:26:18 +0800

    Lab2 init DLNode done

---
+++ lab2/DLNode.java
@@ -0,0 +1,35 @@
+public class DLNode { 
+    int element;
+    DLNode prev;
+    DLNode next;
+    // create a DLNode 
+    DLNode(int element) {
+        this.element = element;
+        this.prev = null;
+        this.prev = null;
+	}
+    // get the value of the element 
+    public int getElement() {
+        return this.element;
+    }
+    // set the value of the element 
+    public void setElement(int element) {
+        this.element = element;
+    }
+    // get the previous DLNode 
+    public DLNode getPrev() {
+        return this.prev;
+    }
+    // set the previous DLNode 
+    public void setPrev(DLNode prev) {
+        this.prev = prev;
+    }
+    // get the next DLNode 
+    public DLNode getNext() {
+        return this.next;
+    }
+    // set the next DLNode 
+    public void setNext(DLNode next) {
+        this.next = next;
+    }
+}
