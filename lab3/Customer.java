ddd683cfefde05f65e2e76e09f78abbddddccede
hdt <p4ngb0y@gmail.com>
2017-10-18 19:14:12 +0800

    add Customer

---
+++ lab3/Customer.java
@@ -0,0 +1,25 @@
+public class Customer {
+    int arrival;
+    int service;
+    // create a Customer
+    public Customer (int arrival, int service) {
+        this.arrival = arrival;
+        this.service = serivce;
+    }
+    // get the arrival time
+    public int getArrival() {
+        return this.arrival;
+    }
+    // set the arrival time
+    public void setArrival (int arrival) {
+        this.arrival = arrival;
+    }
+    // get the service time
+    public int getService() {
+        return this.service;
+    }
+    // set the service time
+    public void setService(int service) {
+        this.service = service;
+    }
+}
